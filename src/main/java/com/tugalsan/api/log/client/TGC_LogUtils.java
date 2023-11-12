3168316b0dcc0875b9d37ba5c033c83c13613bea
tugalsan karabacak <tugalsan@users.noreply.github.com>
2023-11-12 13:23:46 +0300

    .

--- src/main/java/com/tugalsan/api/log/client/TGC_LogUtils.java
+++ src/main/java/com/tugalsan/api/log/client/TGC_LogUtils.java
@@ -1,13 +1,15 @@
 package com.tugalsan.api.log.client;
 
-import elemental2.dom.*;
+//import elemental2.dom.*;
 
 public class TGC_LogUtils {
     
-    public static void plain(CharSequence text) {
-        DomGlobal.console.log(text); 
-    }
+//    public static void plain(CharSequence text) {//NO NEED TO ADD a dep for a func
+//        DomGlobal.console.log(text); 
+//    }
 
+    public static native void plain(CharSequence text) /*-{console.log(String(text));}-*/;
+    
     public static native void error(CharSequence text) /*-{console.log('%c ' + String(text), 'color: red; font-weight: bold; background-color: #242424;');}-*/;
 
     public static native void info(CharSequence text) /*-{console.log('%c ' + String(text), 'color: gray; font-weight: bold; background-color: #242424;');}-*/;
